6db0cd545680fe77763854358a890dcf96f26375
iamrafsanrafi <arafat.yasir1305@gmail.com>
2025-10-15 20:16:59 +0600

    Redux toolkit setup

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -1,4 +1,9 @@
+import { useSelector } from "react-redux"
+
 const RegistrationPage = () => {
+  const userInfo = useSelector(state => state.auth.userInfo);
+
+  console.log(userInfo);
   return (
     <div>This is from registration page</div>
   )
