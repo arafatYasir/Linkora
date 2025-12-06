d0e4c8d69b1c39853015fbc409b5daf85bac8d4c
iamrafsanrafi <arafat.yasir1305@gmail.com>
2025-12-06 09:24:37 +0600

    Mapping nav icons from external js file

---
+++ frontend/src/constants/navOptions.js
@@ -0,0 +1,24 @@
+import { FaHome, FaVideo, FaStore, FaUsers, FaGamepad } from "react-icons/fa"
+
+export const navIcons = [
+    {
+        icon: FaHome,
+        path: "/",
+    },
+    {
+        icon: FaVideo,
+        path: "/video",
+    },
+    {
+        icon: FaStore,
+        path: "/store",
+    },
+    {
+        icon: FaUsers,
+        path: "/friends",
+    },
+    {
+        icon: FaGamepad,
+        path: "/games",
+    },
+]
