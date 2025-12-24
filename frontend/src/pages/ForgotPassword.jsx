bcac44002c371eb8ac034869eb6f78b8a6c05f59
iamrafsanrafi <arafat.yasir1305@gmail.com>
2025-12-24 08:00:48 +0600

    Updated ForgotPassword for common design

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -9,35 +9,53 @@ const ForgotPassword = () => {
     const [user, setUser] = useState({});
 
     const renderComponent = () => {
-        switch(visibility) {
+        switch (visibility) {
             case 0:
                 return <FindAccount setUser={setUser} setVisibility={setVisibility} />
             case 1:
-                if(user) {
+                if (user) {
                     return <ResetPassword user={user} setVisibility={setVisibility} />
                 }
                 setVisibility(0);
                 break;
             case 2:
-                if(user) {
+                if (user) {
                     return <ResetPasswordConfirmation user={user} setVisibility={setVisibility} />
                 }
                 setVisibility(0);
                 break;
             case 3:
-                if(user) {
+                if (user) {
                     return <NewPassword user={user} />
                 }
                 setVisibility(0);
                 break;
-            default: 
+            default:
                 return null;
         }
     }
 
     return (
-        <div>
-            {renderComponent()}
+        <div className="min-h-screen flex items-center justify-center px-4 py-8 bg-bg">
+            <div className="w-full max-w-md">
+                {/* ---- Header ---- */}
+                <div className="text-center mb-8 flex flex-col items-center gap-y-4">
+                    <div className="flex items-center gap-x-2">
+                        <img
+                            className="w-9 h-9"
+                            src="/images/logo.svg"
+                            alt="Logo of Linkora"
+                            loading="lazy"
+                        />
+                        <h1 className="text-4xl font-bold text-primary-hover">Linkora</h1>
+                    </div>
+                </div>
+
+                {/* ---- Card Container ---- */}
+                <div className="rounded-lg p-6 shadow-lg bg-surface border border-border">
+                    {renderComponent()}
+                </div>
+            </div>
         </div>
     )
 }
